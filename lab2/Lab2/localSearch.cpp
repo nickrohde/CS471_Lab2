4910b75a3dbf7962d76607f8f91e136254610920
nickrohde <rohden@cwu.edu>
2018-04-16 11:47:04 -0700

    ls

---
+++ lab2/Lab2/localSearch.cpp
@@ -0,0 +1,28 @@
+#include "localSearch.hpp"
+
+results * localSearch(costFunction f, const std::size_t ui_SIZE, const std::size_t ui_ITERATIONS, double d_min, double d_max, std::vector<double>* x_0)
+{
+	std::vector<double>* vect = new std::vector<double>();
+
+	bool b_continue = true;
+
+	vect = std::move(x_0);
+
+	while (b_continue)
+	{
+		b_continue = false;
+
+
+
+
+
+	} // end while
+
+
+
+	return nullptr;
+}
+
+void getNeighbour(std::vector<double>* vect)
+{
+}
